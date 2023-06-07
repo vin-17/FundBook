0ecd4a478b104fc757ff48e9a81a35a4b5d1b5d2
Vineet Singh <vineet.yo3@gmail.com>
2023-06-08 03:37:12 +0530

    client staged

--- src/components/DisplayCampaigns.jsx
+++ src/components/DisplayCampaigns.jsx
@@ -9,7 +9,7 @@ const DisplayCampaigns = ({ title, isLoading, campaigns }) => {
   
   
   const handleNavigate = (campaign) => {
-    navigate(`/campaign-details/${campaign.title}`, { state: campaign })
+    navigate(`/campaign-details/${campaign.title}`, { state: campaign }) //passing the state as entire campaign
   }
   
   return (
