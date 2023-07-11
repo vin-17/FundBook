0dda0a83bae7e84b44d9e8a7926864c135c40a43
Vineet Singh <vineet.yo3@gmail.com>
2023-07-12 00:14:18 +0530

    some button commits

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,19 +14,10 @@ const Navbar = () => {
 
   return (
     //wrapper div
-    <div className="flex md:flex-row flex-col-reverse justify-between mb-[35px] gap-6">
-      <div className="lg:flex-1 flex flex-row max-w-[458px] py-2 pl-4 pr-2 h-[52px] bg-[#1c1c24] rounded-[100px]">
-        {/* search for campaigns input */}
-        <input type="text" placeholder="Search for campaigns" className="flex w-full font-epilogue font-normal text-[14px] placeholder:text-[#4b5264] text-white bg-transparent outline-none" />
-        
-        {/* wrapper for the search */}
-        <div className="w-[72px] h-full rounded-[20px] bg-[#4acd8d] flex justify-center items-center cursor-pointer">
-          <img src={search} alt="search" className="w-[15px] h-[15px] object-contain"/>
-        </div>
-      </div>
+    <div className="flex  md:flex-row flex-col-reverse justify-between mb-[45px] gap-6">
       
       {/* button thats gonna appear on the right side */}
-      <div className="sm:flex hidden flex-row justify-end gap-4">
+      <div className="ml-auto sm:flex hidden flex-row justify-end gap-4">
         <CustomButton 
           btnType="button"
           title={address ? 'Create a campaign' : 'Connect'}
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,7 +3,7 @@ import { Link, useNavigate } from 'react-router-dom';
 
 import { logo, sun } from '../assets';
 import { navlinks } from '../constants';
-
+//to show the highlighted button
 const Icon = ({ styles, name, imgUrl, isActive, disabled, handleClick }) => (
   <div className={`w-[48px] h-[48px] rounded-[10px] ${isActive && isActive === name && 'bg-[#2c2f32]'} flex justify-center items-center ${!disabled && 'cursor-pointer'} ${styles}`} onClick={handleClick}>
     {!isActive ? (
@@ -20,7 +20,7 @@ const Sidebar = () => {
 
   return (
     //wrapper for entire sidebar
-    <div className="flex justify-between items-center flex-col sticky top-5 h-[93vh]">
+    <div className="flex justify-between items-center flex-col sticky top-5">
       <Link to="/"> {/*home page link */}
         <Icon styles="w-[52px] h-[52px] bg-[#2c2f32]" imgUrl={logo} />
       </Link>
@@ -43,7 +43,7 @@ const Sidebar = () => {
           ))}
         </div>
 
-        <Icon styles="bg-[#1c1c24] shadow-secondary" imgUrl={sun} />
+        
       </div>
     </div>
   )
